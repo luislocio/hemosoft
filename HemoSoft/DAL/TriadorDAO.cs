27ec9b1a6cb2abb0106ca5d8ea0e70a28b24578c
yuriaraujo <yuri.alvesaraujo88@gmail.com>
2019-09-07 16:59:24 -0300

    Criado o metodo para buscar o Triador pela matricula

--- HemoSoft/DAL/TriadorDAO.cs
+++ HemoSoft/DAL/TriadorDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HemoSoft.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace HemoSoft.DAL
 {
     class TriadorDAO
     {
+        private static Context ctx = SingletonContext.GetInstance();
+        public static void CadastrarTriador(Triador t)
+        {
+            ctx.Triadores.Add(t);
+            ctx.SaveChanges();
+        }
+
+        public static Triador BuscarTriadorPorMatricula(Triador t)
+        {
+            return ctx.Triadores.FirstOrDefault
+                (x => x.Matricula.Equals(t.Matricula));
+        }
     }
 }
