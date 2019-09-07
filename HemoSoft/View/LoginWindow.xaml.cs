961b4067b8e31d3fdabab6c5eb43095aff36cc0a
luislocio <luislocio@gmail.com>
2019-09-07 19:13:16 -0300

    Implementado login de usuarios

--- HemoSoft/View/LoginWindow.xaml.cs
+++ HemoSoft/View/LoginWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using HemoSoft.DAL;
+using HemoSoft.Model;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace HemoSoft.View
 {
@@ -10,6 +13,78 @@ namespace HemoSoft.View
         public LoginWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            bool usuarioAutenticado = false;
+
+            if (txtUsuario.Text.Equals("") || txtSenha.Equals(""))
+            {
+                MessageBox.Show("Campo vazio!");
+            }
+            else
+            {
+                if (txtUsuario.Text.Length == 14)
+                {
+                    usuarioAutenticado = AutenticarSolicitante(txtUsuario, txtSenha);
+                } else
+                {
+                    usuarioAutenticado = AutenticarTriador(txtUsuario, txtSenha);
+                }
+                
+            }
+
+            if (usuarioAutenticado == true)
+            {
+                MainWindow main = new MainWindow();
+                App.Current.MainWindow = main;
+                this.Close();
+                main.Show();
+            } else
+            {
+                MessageBox.Show("Dados Inválidos.");
+            }
+        }
+
+        private bool AutenticarTriador(TextBox txtUsuario, PasswordBox txtSenha)
+        {
+            Triador triadorBusca = new Triador
+            {
+                Matricula = txtUsuario.Text
+            };
+
+            Triador triadorResultado = TriadorDAO.BuscarTriadorPorMatricula(triadorBusca);
+            if (triadorResultado != null)
+            {
+                if (triadorResultado.Matricula.Equals(txtUsuario.Text) && triadorResultado.Senha.Equals(txtSenha.Password))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        private bool AutenticarSolicitante(TextBox txtUsuario, PasswordBox txtSenha)
+        {
+            Triador triadorBusca = new Triador
+            {
+                Matricula = txtUsuario.Text
+            };
+
+
+            Triador triadorResultado = TriadorDAO.BuscarTriadorPorMatricula(triadorBusca);
+            if (triadorResultado != null)
+            {
+                if (triadorResultado.Matricula.Equals(txtUsuario.Text) && triadorResultado.Senha.Equals(txtSenha.Password))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
